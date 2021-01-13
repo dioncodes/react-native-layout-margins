440421aedf3c5ff88e9f7f6ea28c88cbc66fec4d
Dion Purushotham <mail@dion.codes>
2021-01-13 11:10:09 +0100

    feat: view component for easier usage
    Signed-off-by: Dion Purushotham <mail@dion.codes>

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,15 @@ declare module '@dioncodes/react-native-layout-margins' {
         right: number,
         bottom: number,
         top: number,
-    }>;
+	}>;
+
+	export const ContentInsetView: React.FC<{
+		children: React.ReactNode,
+		horizontal?: boolean,
+		vertical?: boolean,
+		left?: boolean,
+		right?: boolean,
+		top?: boolean,
+		bottom?: boolean,
+	}>;
 }
